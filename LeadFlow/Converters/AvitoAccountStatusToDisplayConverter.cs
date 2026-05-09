59bab15a6a04416b250c1253c7ae1340cb64b32e
Никита <@AORUS-PC>
2026-05-10 00:23:03 +0500

    Обновлены стили и разметка в представлениях JournalView, MonitoringView, StatisticsHistoryView и SettingsWindow для улучшения пользовательского интерфейса. Добавлены новые стили для кнопок и бейджей, а также изменены параметры высоты элементов. Обновлены ресурсы и привязки для интеграции новых стилей и улучшения взаимодействия с пользователем.

---
+++ LeadFlow/Converters/AvitoAccountStatusToDisplayConverter.cs
@@ -0,0 +1,26 @@
+using System.Globalization;
+using System.Windows.Data;
+using LeadFlow.Models;
+
+namespace LeadFlow.Converters;
+
+public sealed class AvitoAccountStatusToDisplayConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object parameter, CultureInfo culture) =>
+        value is AvitoAccountStatus s ? ToDisplay(s) : string.Empty;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        Binding.DoNothing;
+
+    private static string ToDisplay(AvitoAccountStatus s) => s switch
+    {
+        AvitoAccountStatus.NotConfigured => "Не настроен",
+        AvitoAccountStatus.RequiresLogin => "Нужно войти",
+        AvitoAccountStatus.Authorized => "Готов к работе",
+        AvitoAccountStatus.Monitoring => "Мониторится",
+        AvitoAccountStatus.Paused => "Приостановлен",
+        AvitoAccountStatus.RequiresManualAction => "Нужно действие",
+        AvitoAccountStatus.Error => "Ошибка",
+        _ => s.ToString()
+    };
+}
